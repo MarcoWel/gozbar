7c713fbdac5196e0dab69dff7e339f957bb903f3
MarcoWel <mw@welectron.com>
2024-04-30 15:34:13 +0200

    Adding more functions, change naming according to current scheme

--- symbol.go
+++ symbol.go
@@ -57,32 +57,59 @@ func (s *Symbol) Each(f func(string)) {
 	}
 }
 
+// Type retrieves type of decoded symbol.
+func (s *Symbol) Type() int {
+	return int(C.zbar_symbol_get_type(s.c_symbol))
+}
+
+// Name retrieves string name for symbol encoding.
+func (s *Symbol) Name() string {
+	cstr := C.zbar_get_symbol_name(C.zbar_symbol_get_type(s.c_symbol))
+	return C.GoString(cstr)
+}
+
+// AddonName retrieves string name for addon encoding
+func (s *Symbol) AddonName() string {
+	return C.GoString(C.zbar_get_addon_name(C.zbar_symbol_get_type(s.c_symbol)))
+}
+
+// Quality retrieves a symbol confidence metric.
+// Returns an unscaled, relative quantity: larger values are better than smaller values,
+// where "large" and "small" are application dependent.
+// Note:
+//
+//	expect the exact definition of this quantity to change as the metric is refined.
+//	Currently, only the ordered relationship between two values is defined and will remain stable in the future
+func (s *Symbol) Quality() int {
+	return int(C.zbar_symbol_get_quality(s.c_symbol))
+}
+
 // Retrieve the number of points in the location polygon.
 // The location polygon defines the image area that the symbol was extracted from.
 // Returns the number of points in the location polygon
 // Note:
 //
 //	this is currently not a polygon, but the scan locations where the symbol was decoded
-func (s *Symbol) GetLocSize() uint {
+func (s *Symbol) LocSize() uint {
 	return uint(C.zbar_symbol_get_loc_size(s.c_symbol))
 }
 
-// GetLocX retrieves location polygon x-coordinates.
+// LocX retrieves location polygon x-coordinates.
 // Points are specified by 0-based index.
 // Returns:
 //
 //	the x-coordinate for a point in the location polygon.
 //	-1 if index is out of range
-func (s *Symbol) GetLocX(index uint) int {
+func (s *Symbol) LocX(index uint) int {
 	return int(C.zbar_symbol_get_loc_x(s.c_symbol, C.unsigned(index)))
 }
 
-// GetLocY retrieves location polygon y-coordinates.
+// LocY retrieves location polygon y-coordinates.
 // Points are specified by 0-based index.
 // Returns:
 //
 //	the y-coordinate for a point in the location polygon.
 //	-1 if index is out of range
-func (s *Symbol) GetLocY(index uint) int {
+func (s *Symbol) LocY(index uint) int {
 	return int(C.zbar_symbol_get_loc_y(s.c_symbol, C.unsigned(index)))
 }
