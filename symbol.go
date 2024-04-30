b4917834a5e214e4b6b61a07ba0120c663634f00
MarcoWel <mw@welectron.com>
2024-04-30 15:44:27 +0200

    Fix

--- symbol.go
+++ symbol.go
@@ -57,11 +57,6 @@ func (s *Symbol) Each(f func(string)) {
 	}
 }
 
-// Type retrieves type of decoded symbol.
-func (s *Symbol) Type() int {
-	return int(C.zbar_symbol_get_type(s.symbol))
-}
-
 // Name retrieves string name for symbol encoding.
 func (s *Symbol) Name() string {
 	cstr := C.zbar_get_symbol_name(C.zbar_symbol_get_type(s.symbol))
